2e1f3e8c40d2da4678c1a55f307d1ec61bda8d7a
Tahara Begum <tahara.b3gum123@gmail.com>
2013-07-12 11:45:00 -0400

    Idea Box

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
+require './idea'
+
+
+
+
 class IdeaBoxApp < Sinatra::Base
-  get '/' do
-    "Hello, World!"
-  end
+ get '/' do
+  erb :index, locals: {ideas: Idea.all}
 end
+  not_found do
+    erb :error
+  end
+   # ...
+
+
+	post '/' do
+  idea = Idea.new(params['idea_title'], params['idea_description'])
+  idea.save
+  redirect '/'
+end
+
+
+end
+
+
 
 
